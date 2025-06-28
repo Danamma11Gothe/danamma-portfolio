684f80a291231268c02fb06214786da84d5f2d66
Danu <danammagothe@gmail.com>
2025-06-28 13:38:34 +0530

    Fix merge conflicts in postcss.config.cjs and about.tsx

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-=======
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
->>>>>>> 9b41d0b926c65158e8f534852ba6a5347c9138a9
-};
+}; 
